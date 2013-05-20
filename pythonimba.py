0160dc4e330c02c00d229483d465071c55f2c173
Chng Guo Hao <chngguohao@gmail.com>
2013-05-20 18:02:59 +0800

    i duno what i did

--- pythonimba.py
+++ pythonimba.py
@@ -4,10 +4,10 @@ radius= float(input("Enter the radius: ")) 	 #input always string
 circumference = 2 * math.pi * radius		 #every var in python is treated as object
 area = math.pi * radius**2			 # ** = power
 print()						 # line break/ empty line
-print("The circumference of circle is: ", circumference,\  # \ to continue next line
-	", and the area is: ", area)
+print("The circumference of circle is: ", circumference,\
+	", and the area is: ", area)             # \ to continue next line
 
-print "*" 20
+print "*" * 20
 print()
 print "\tLegend of Zelda"
 print()
@@ -32,9 +32,9 @@ for i in range(5):
 	for j in range(5):
 		print i + j
 
-list = [1,2,3,4,5,6,7,8,9,10]
-print list[1:9:2]				 #start end increment
-print list[::-1]				 #default 1st last 1
+list = [1,2,3,4,5,6,7,8,9,10]                    #start end increment
+print list[1:9:2]				 #default 1st last 1
+print list[::-1]				 #start from the back
 print list[2::4]
 print list
 
